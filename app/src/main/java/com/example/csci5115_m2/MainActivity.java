72ec365a1dea08a8994475b60897be97b7b33fe0
Stephen-Fan <fanqinghong13@gmail.com>
2022-03-30 14:11:37 -0500

    breakfast, lunch page
    Finish breakfast page view but not clickable. Finish lunch page but unable to be shown when click on lunch button.

--- app/src/main/java/com/example/csci5115_m2/MainActivity.java
+++ app/src/main/java/com/example/csci5115_m2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.csci5115_m2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,7 +16,7 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.csci5115_m2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+//    private Button button;
     private ActivityMainBinding binding;
 
     @Override
@@ -22,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -32,6 +37,18 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+//
+//        button = (Button) findViewById(R.id.button3);
+//        button.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                openLunchActivity();
+//            }
+//        });
     }
 
+//    public void goToLunchPage() {
+//        Intent intent = new Intent(this, LunchActivity.class);
+//        startActivity(intent);
+//    }
 }
