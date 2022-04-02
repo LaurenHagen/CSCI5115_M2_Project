348377ed5da4cf7963c703e955edd317b7a2f569
LaurenHagen <56652938+LaurenHagen@users.noreply.github.com>
2022-04-02 10:26:43 -0500

    change location of file

--- app/src/main/java/com/example/csci5115_m2/SplashActivity.java
+++ app/src/main/java/com/example/csci5115_m2/SplashActivity.java
@@ -1,6 +1,5 @@
-package com.example.csci5115_m2.ui;
+package com.example.csci5115_m2;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
